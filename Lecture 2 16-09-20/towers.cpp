ecf75b29e8d1591d03705a0e97d83de1cb498c9b
FilippoLari <62613534+FilippoLari@users.noreply.github.com>
2021-01-27 16:10:13 +0100

    Add files via upload

---
+++ Lecture 2 16-09-20/towers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+/*
+	The algorithm sorts the lengths then
+	each sequence of single or repeating lengths is counted as
+	one tower keeping track of the maximal height.
+	
+	Time:  O(nlogn) because of sorting.
+	Space: O(1) assuming the sort procedure doesn't use any extra space. 
+*/
+
+int main()
+{
+    int N, i;
+    
+    cin >> N;
+    
+    int l[N];
+    
+    for(i=0; i<N; i++)
+        cin >> l[i];
+        
+    sort(l, l+N);
+    
+    int max=1, h=1, bars=1;
+    
+    for(i=1; i<N; i++)
+    {
+        if(l[i]!=l[i-1])
+        {
+            if(max<h) max=h;
+            bars++;
+            h = 1;
+        }
+        else
+            h++;
+    }
+    
+    if(max<h) max=h;
+    
+    cout << max << " " << bars;
+    
+    return 0;
+}
